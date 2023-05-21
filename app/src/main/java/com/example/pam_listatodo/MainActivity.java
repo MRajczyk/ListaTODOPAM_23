2c32e2c97b0a4e96aec54053a16684791a6fb2fb
Mikołaj Rajczyk <mikolajrajczyk01@gmail.com>
2023-05-21 22:54:54 +0200

    added preferences and setting dafault values

--- app/src/main/java/com/example/pam_listatodo/MainActivity.java
+++ app/src/main/java/com/example/pam_listatodo/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.pam_listatodo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceManager;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
     }
 
     @Override
