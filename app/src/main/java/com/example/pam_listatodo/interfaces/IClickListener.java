3a64c940c55ff4e493137881602fc1643578b80a
Mikołaj Rajczyk <mikolajrajczyk01@gmail.com>
2023-05-24 01:16:23 +0200

    added click interface

---
+++ app/src/main/java/com/example/pam_listatodo/interfaces/IClickListener.java
@@ -0,0 +1,7 @@
+package com.example.pam_listatodo.interfaces;
+
+public interface IClickListener {
+    void onClickItem(int position);
+    void onClickButtonFinish(int position);
+    void onClickButtonDelete(int position);
+}
