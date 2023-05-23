a9c8e096fbe98b6da048be2f168ec69eda709a83
Mikołaj Rajczyk <mikolajrajczyk01@gmail.com>
2023-05-24 01:18:48 +0200

    implemented recycler fragment and respective adapter

---
+++ app/src/main/java/com/example/pam_listatodo/adapters/TaskRecyclerViewAdapter.java
@@ -0,0 +1,92 @@
+package com.example.pam_listatodo.adapters;
+
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.pam_listatodo.R;
+import com.example.pam_listatodo.interfaces.IClickListener;
+import com.example.pam_listatodo.models.Status;
+import com.example.pam_listatodo.models.Task;
+import com.google.android.material.chip.Chip;
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+public class TaskRecyclerViewAdapter extends RecyclerView.Adapter<TaskRecyclerViewAdapter.ViewHolder> {
+
+    private final IClickListener clickListener;
+    private final List<Task> tasks;
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
+
+    public TaskRecyclerViewAdapter(List<Task> tasks, IClickListener clickListener) {
+        this.tasks = tasks;
+        this.clickListener = clickListener;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.task_row, parent, false);
+        return new ViewHolder(v, this.clickListener);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        holder.taskTitle.setText(tasks.get(position).getTaskTitle());
+        holder.taskCategory.setText(tasks.get(position).getTaskCategory().toString());
+        holder.taskDeadline.setText(Instant.ofEpochSecond(tasks.get(position).getTaskDueTime()).atZone(ZoneId.systemDefault()).format(formatter));
+        if (tasks.get(position).getTaskStatus().equals(Status.COMPLETE)) {
+            holder.completeTask.setChipBackgroundColor(ColorStateList.valueOf(ContextCompat.getColor(holder.completeTask.getContext(), R.color.green)));
+            holder.completeTask.setText("Done!");
+        }
+        else {
+            holder.completeTask.setChipBackgroundColor(ColorStateList.valueOf(ContextCompat.getColor(holder.completeTask.getContext(), R.color.lightgrey)));
+            holder.completeTask.setText("Pending");
+        }
+        if (tasks.get(position).getAttachmentURI() != null && !tasks.get(position).getAttachmentURI().equals(""))
+            holder.taskAttachment.setVisibility(View.VISIBLE);
+        else
+            holder.taskAttachment.setVisibility(View.GONE);
+    }
+
+    @Override
+    public int getItemCount() {
+        return tasks.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView taskTitle;
+        TextView taskDeadline;
+        TextView taskCategory;
+        Chip completeTask;
+        ImageView taskAttachment;
+        ImageView deleteTask;
+
+        ViewHolder(View itemView, IClickListener clickListener) {
+            super(itemView);
+            taskTitle = itemView.findViewById(R.id.task_title);
+            taskCategory = itemView.findViewById(R.id.task_category);
+            taskDeadline = itemView.findViewById(R.id.task_deadline);
+            completeTask = itemView.findViewById(R.id.task_done_chip);
+            taskAttachment = itemView.findViewById(R.id.task_attachment);
+            deleteTask = itemView.findViewById(R.id.task_delete);
+
+            deleteTask.setOnClickListener(view -> clickListener.onClickButtonDelete(getAdapterPosition()));
+            completeTask.setOnClickListener(view -> clickListener.onClickButtonFinish(getAdapterPosition()));
+            itemView.setOnClickListener(v -> clickListener.onClickItem(getAdapterPosition()));
+        }
+    }
+}
--- app/src/main/java/com/example/pam_listatodo/fragments/RecyclerViewFragment.java
+++ app/src/main/java/com/example/pam_listatodo/fragments/RecyclerViewFragment.java
@@ -1,6 +1,120 @@
 package com.example.pam_listatodo.fragments;
 
-import androidx.fragment.app.Fragment;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class RecyclerViewFragment extends Fragment {
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.pam_listatodo.MainActivity;
+import com.example.pam_listatodo.R;
+import com.example.pam_listatodo.adapters.TaskRecyclerViewAdapter;
+import com.example.pam_listatodo.database.DatabaseTaskHandler;
+import com.example.pam_listatodo.interfaces.IClickListener;
+import com.example.pam_listatodo.models.Status;
+import com.example.pam_listatodo.models.Task;
+
+import java.util.List;
+
+public class RecyclerViewFragment extends Fragment implements IClickListener {
+
+    private View view;
+    private EditText editTextFindTask;
+    private Button sortButton;
+    private List<Task> taskData;
+    private TaskRecyclerViewAdapter adapter;
+    private DatabaseTaskHandler dbHandle;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_recycler_tasks, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        this.view = view;
+        this.sortButton = view.findViewById(R.id.sortButton);
+        taskData = ((MainActivity)requireActivity()).getAllTasks();
+        editTextFindTask = view.findViewById(R.id.task_filter);
+        this.dbHandle = ((MainActivity)requireActivity()).getDb();
+        editTextFindTask.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                taskData = ((MainActivity)requireActivity()).getTaskByTitle(charSequence.toString());
+                startRecyclerView();
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                //stub
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                //stub
+            }
+        });
+
+        sortButton.setOnClickListener(v -> {
+            //todo: sort by most urgent task
+        });
+
+        startRecyclerView();
+    }
+
+    private void startRecyclerView() {
+        RecyclerView recyclerView = view.findViewById(R.id.recycler_view_tasks);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        adapter = new TaskRecyclerViewAdapter(taskData, this);
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onClickItem(int position) {
+        Bundle bundle = new Bundle();
+        bundle.putSerializable("data", taskData.get(position));
+        Fragment fragment = new MoreDetailsFragment();
+        fragment.setArguments(bundle);
+        getParentFragmentManager()
+                .beginTransaction()
+                .replace(R.id.mainActivity, fragment)
+                .setReorderingAllowed(true)
+                .commit();
+    }
+
+    //todo: see how it really works xd
+    @Override
+    public void onClickButtonFinish(int position) {
+        Task updatedTask = new Task(taskData.get(position));
+        updatedTask.setTaskStatus(updatedTask.getTaskStatus() == Status.COMPLETE ? Status.PENDING : Status.COMPLETE);
+        System.out.println(updatedTask.getTaskStatus().toString());
+        this.dbHandle.updateTask(updatedTask);
+        taskData.set(position, updatedTask); //po bozemu ale nie dziala bo nie refreshuje filtru :( (moze i dobrze?)
+        adapter.notifyItemChanged(position);
+
+        //to nizej dziala, ale czy my chcemy zeby tak to dzialalo?
+//        this.taskData = ((MainActivity) requireActivity()).getAllTasks();
+//        startRecyclerView();
+    }
+
+    //todo: lol
+    @Override
+    public void onClickButtonDelete(int position) {
+        ((MainActivity) requireActivity()).getDb().deleteTask(taskData.get(position));
+        taskData.remove(position);
+        adapter.notifyItemRemoved(position);
+    }
 }
