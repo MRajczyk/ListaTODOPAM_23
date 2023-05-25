8ca3b6d5813e6caf3f2f5468fed1953a1d3c9448
MRajczyk <mikolajrajczyk01@gmail.com>
2023-05-25 15:27:41 +0200

    attachment deletion on task deletion

--- app/src/main/java/com/example/pam_listatodo/database/DatabaseTaskHandler.java
+++ app/src/main/java/com/example/pam_listatodo/database/DatabaseTaskHandler.java
@@ -14,6 +14,7 @@ import com.example.pam_listatodo.models.Category;
 import com.example.pam_listatodo.models.Status;
 import com.example.pam_listatodo.models.Task;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -135,6 +136,11 @@ public class DatabaseTaskHandler extends SQLiteOpenHelper {
     }
 
     public void deleteTask(Task task) {
+        if(task.getTaskAttachmentURI() != null && !task.getTaskAttachmentURI().equals("")) {
+            File attachment = new File(task.getTaskAttachmentURI());
+            attachment.delete();
+        }
+
         SQLiteDatabase db = this.getWritableDatabase();
         db.delete(TABLE_NAME, ID + " = ?",
                 new String[]{String.valueOf(task.getId())});
